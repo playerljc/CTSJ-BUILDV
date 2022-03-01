0c5f3660ebc5c1a80960482ba0e9e84ecc9d1daf
zhiqian.liu <1qaz@WSX>
2022-03-01 16:49:37 +0800

    chore:(CTSJ-BUILD-PACKAGE)微调

--- packages/CTSJ-BUILDV-PACKAGE/lib/component.js
+++ packages/CTSJ-BUILDV-PACKAGE/lib/component.js
@@ -1,2 +1,2 @@
-import Vue from"vue";export default{component:Vue.extend({methods:{renderInner:function(e){return e("div",["我是JSX写的组件666"])}},render:function(e){return this.renderInner(e)}}),name:"my-component"};
+export default{methods:{renderInner:function(n){return n("div",["我是JSX写的组件666"])}},render:function(n){return this.renderInner(n)},name:"my-component"};
 //# sourceMappingURL=component.js.map
