a60f6bb078dc8d02ab276cfb252801f7670956a8
zhiqian.liu <1qaz@WSX>
2022-03-03 08:28:45 +0800

    chore:format

--- packages/CTSJ-BUILDV/webpackconfig/config/projectDisableStrict.js
+++ packages/CTSJ-BUILDV/webpackconfig/config/projectDisableStrict.js
@@ -4,7 +4,7 @@ const { isDev } = require('../../util');
  * 禁用use strict
  * @param webpackConfig
  */
-module.exports = function ({ webpackConfig}) {
+module.exports = function ({ webpackConfig }) {
   const useIndex = isDev() ? 0 : 1;
 
   webpackConfig.module.rules[1].use[useIndex].query.plugins.push([
